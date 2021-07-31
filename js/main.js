1817e2b83450aa9097d8fd1e56a6d0176ab36c6c
Sunil Talekar <sunil.talekar@infobeans.com>
2021-07-31 14:41:20 +0530

    updates

--- js/main.js
+++ js/main.js
@@ -15,7 +15,66 @@ $(function(){
             current : ''
         }
     });
+    // $("form[name='registration']").validate({
+    //     rules: { 
+    //       fullname : "required",
+    //       address: "required",
+    //       email: {
+    //         required: true,
+    //         email: true
+    //       },
+         
+    //     },
+    //     // Specify validation error messages
+    //     messages: {
+    //       name: "Please enter your firstname",
+    //       addres: "Please enter your address",
+
+    //       email: "Please enter a valid email address"
+    //     },
+
+    //     submitHandler: function(form) {
+    //       form.submit();
+    //     }
+    //   });
     $('#dob').dcalendarpicker();
 });
+
+function show_desc_1()
+	{
+		document.getElementById('text_desc_1').style.display="block";
+    }
+    function show_desc_2()
+	{
+		document.getElementById('text_desc_2').style.display="block";
+    }
+    function show_desc_3()
+	{
+		document.getElementById('text_desc_3').style.display="block";
+    }
+    function show_desc_4()
+	{
+		document.getElementById('text_desc_4').style.display="block";
+    }
+    
+    
+
+    function hide_desc_1()
+	{
+	document.getElementById('text_desc_1').style.display="none";
+}
+    function hide_desc_2()
+    {
+    document.getElementById('text_desc_2').style.display="none";
+}
+    function hide_desc_3()
+	{
+        document.getElementById('text_desc_3').style.display="none";
+    }
+    function hide_desc_4()
+	{
+        document.getElementById('text_desc_4').style.display="none";
+    }
+    
 
 
